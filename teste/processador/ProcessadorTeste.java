75b4acb650161092dd5c8583093b47e7f506c0e1
VictorMatheus819 <victormatheus819@gmail.com>
2021-06-27 15:38:22 -0300

    Caso de debito de fatura  falho

--- teste/processador/ProcessadorTeste.java
+++ teste/processador/ProcessadorTeste.java
@@ -1,9 +1,13 @@
 package processador;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import boleto.Boleto;
 import fatura.Fatura;
@@ -27,11 +31,29 @@ public class ProcessadorTeste {
 	@Test 
 	public void TestaAssociacaoFatura() {
 		 Fatura fat = new Fatura (new Date(),"carlos",170.0);
-		 Processador processador   = new Processador();
-		 
+		 Processador processador = new Processador();
 		 processador.setFatura(fat);
-		 
 		 Assert.assertEquals(fat,processador.getFatura());
+		 
+	}
+	
+	@Test 
+	public void TestaDebitoDeFatura() {
+		 Fatura fat = new Fatura (new Date(),"carlos",170.0);
+		 Boleto bol1 = new Boleto(new Date(),"16ji",80.0);
+		 Boleto bol2 = new Boleto(new Date(),"17lo",90.0);
+		 ArrayList <Boleto> list = new ArrayList<Boleto>();
+		 Processador processador = new Processador();
+		 list.add(bol1);
+		 list.add(bol2);
+		 processador.ProcessaBoletos(list);
+		 processador.setFatura(fat);
+		 Assertions.assertAll("pag",
+					() -> assertEquals("PAGA", fat.getSituacao()),
+					() -> assertEquals(150.6, pag.getValorPago()),
+					( )-> assertTrue(pag.getData() != null)
+					);
+		 Assert.assertEquals("Boleto",fat.getSituacao());
 		 
 	}
 	
